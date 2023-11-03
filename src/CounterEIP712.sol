39f1ebe9009683f66370c6aa565ccba167e5ef96
Jaka Potokar <cc.jakapotokar@gmail.com>
2023-11-03 10:28:20 +0100

    updated CounterEIP712

--- src/CounterEIP712.sol
+++ src/CounterEIP712.sol
@@ -7,15 +7,25 @@ import {IERC1271} from "./interface/IERC1271.sol";
 
 /**
  * @title CounterEIP712
- * @dev A smart contract that allows secure updates of a number using the EIP-712 standard.
- * The contract is owned by an address that has the authority to verify and set the number.
+ * @dev A smart contract that enables secure updates of a number using the EIP-712 standard.
+ * This contract is owned by an address with the authority to verify and set the number.
  */
 contract CounterEIP712 is Ownable {
     // EIP-712 domain separator
-    bytes32 private DOMAIN_SEPARATOR;
+    bytes32 private immutable DOMAIN_SEPARATOR;
 
-    // Number, which can get changed with setNumber
+    // The number, which can be changed using the setNumber function
     uint256 public number;
+
+    // Type hash for the EIP-712 domain separator
+    bytes32 private constant TYPE_HASH =
+        keccak256(
+            "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+        );
+
+    // Type hash for the setNumber function
+    bytes32 private constant SETNUMBER_TYPEHASH =
+        keccak256("SetNumber(uint256 value)");
 
     /**
      * @dev Constructor to initialize the contract owner and set the EIP-712 domain separator.
@@ -25,11 +35,9 @@ contract CounterEIP712 is Ownable {
         require(_addr.code.length > 0, "Not a contract");
         DOMAIN_SEPARATOR = keccak256(
             abi.encode(
-                keccak256(
-                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-                ),
+                TYPE_HASH,
                 keccak256(bytes("countereip712")),
-                keccak256("\x01"), // Ethereum prefix for message format
+                keccak256("\x01"), // Ethereum prefix for the message format
                 block.chainid,
                 address(this)
             )
@@ -37,17 +45,28 @@ contract CounterEIP712 is Ownable {
     }
 
     /**
-     * @dev Hash a new number to be used for signature verification.
-     * @param _number The new number data.
+     * @dev Compute the struct hash of a setNumber request to be used for signature verification.
+     * @param value The new number to be set.
      * @return The hashed message that will be signed.
+     */
+    function setNumber_structHash(
+        uint256 value
+    ) internal pure returns (bytes32) {
+        return keccak256(abi.encode(SETNUMBER_TYPEHASH, value));
+    }
+
+    /**
+     * @dev Compute the EIP-712 digest for signature verification.
+     * @param _number The new number data.
+     * @return The EIP-712 digest for the given parameters.
      */
     function hashNewNumber(uint256 _number) public view returns (bytes32) {
         return
             keccak256(
                 abi.encodePacked(
-                    "\x19\x01", // Ethereum prefix for message format
+                    "\x19\x01", // Ethereum prefix for the message format
                     DOMAIN_SEPARATOR,
-                    keccak256(abi.encode(_number))
+                    setNumber_structHash(_number)
                 )
             );
     }
@@ -61,7 +80,8 @@ contract CounterEIP712 is Ownable {
         bytes32 dataHash = hashNewNumber(_newNumber);
         require(
             IERC1271(owner()).isValidSignature(dataHash, signature) ==
-                IERC1271.isValidSignature.selector
+                IERC1271.isValidSignature.selector,
+            "Invalid signature"
         );
         number = _newNumber;
     }
